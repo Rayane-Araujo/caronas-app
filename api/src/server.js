60777795a99465a1407dd0d73c867d2032455821
Arthur Telles <arthurtelles19@gmail.com>
2023-12-11 15:15:50 -0300

    CRUD

--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
-
-
-
-
-
 app.listen("3333", function () {
-    console.log("server rodando");
-  });
-  
+  console.log("server rodando");
+});
+
+
+
+app.post("/users", function (req, res) {
+  res.send("Usuario Criado");
+});
+
+app.get('/users', function (req, res) {
+  res.send('GetAll');
+});
+
+app.put('/users', function (req, res) {
+  res.send('Atualizando usuário');
+});
+
+app.delete('/users', function (req, res) {
+  res.send('usuário deletado');
+});
