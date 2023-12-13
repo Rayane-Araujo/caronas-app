140a048fade73eb753519b50fd0cb2355a072bdf
Arthur Telles <arthurtelles19@gmail.com>
2023-12-13 15:16:22 -0300

    crud para procura, oferecer e pedir carona

--- api/src/server.js
+++ api/src/server.js
@@ -11,7 +11,7 @@ app.listen("3333", function () {
 });
 
 
-
+/*--------------------------USERS----------------------------*/
 app.post("/users", function (req, res) {
   res.send("Usuario Criado");
 });
@@ -26,4 +26,46 @@ app.put('/users', function (req, res) {
 
 app.delete('/users', function (req, res) {
   res.send('usuário deletado');
+});
+
+/*--------------------------search----------------------------*/
+
+app.get('/search', function (req, res) {
+  res.send("pegando publicações");
+});
+
+/* -------------------------offer---------------------------*/
+
+app.post("/offer", function (req, res) {
+  res.send("oferecer carona Criado");
+});
+
+app.get("/offer", function (req, res) {
+  res.send("pegando ofertas de Carona");
+});
+
+app.put("/offer", function (req, res) {
+  res.send("atualizando ofertas");
+});
+
+app.delete("offer", function (req, res) {
+  res.send("deletendo oferta");
+});
+
+/* -------------------------request---------------------------*/
+
+app.post("/request", function (req, res) {
+  res.send("pedido de carona criado");
+});
+
+app.get("/request", function (req, res) {
+  res.send("pegando os pedidos de carona");
+});
+
+app.put("/request", function (req, res) {
+  res.send("atualizando pedido de carona");
+});
+
+app.delete("/request", function (req, res) {
+  res.send("deletendo pedido de carona");
 });
