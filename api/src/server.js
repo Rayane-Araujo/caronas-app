a197d4231836b0af80ea2173d608a0d397f6c234
Arthur Telles <arthurtelles19@gmail.com>
2023-12-22 12:27:48 -0300

    tabela pedir e oferecer

--- api/src/server.js
+++ api/src/server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 (async () => {
   if ((await knex.schema.hasTable("users")) == false) {
     await knex.schema.createTable("users", function (table) {
@@ -14,44 +13,64 @@ app.use(cors());
       table.string("name");
       table.string("email");
       table.string("telephone");
-      table.date("date");
+      table.date("date_birth");
     });
 
     console.log("criado a tabela users");
   }
+
+  if ((await knex.schema.hasTable("offerRide")) == false) {
+    await knex.schema.createTable("offerRide", function (table) {
+      table.increments("id").primary();
+      table.string("origin");
+      table.string("destination");
+      table.dateTime("time");
+      table.integer("price");
+      table.date("date");
+      table.integer("telephone");
+      table.string("description");
+    });
+    console.log("criado tabela offerRide");
+  }
+
+  if ((await knex.schema.hasTable("requestRide")) == false) {
+    await knex.schema.createTable("requestRide", function (table) {
+      table.increments("id").primary();
+      table.string("origin");
+      table.string("destination");
+      table.dateTime("time");
+      table.date("date");
+      table.integer("telephone");
+      table.string("description");
+    });
+  }
+  console.log("criado a tabela requestRide");
 })();
-
-
-
-
-
-
 
 app.listen("3333", function () {
   console.log("server rodando");
 });
-
 
 /*--------------------------USERS----------------------------*/
 app.post("/users", function (req, res) {
   res.send("Usuario Criado");
 });
 
-app.get('/users', function (req, res) {
-  res.send('GetAll');
+app.get("/users", function (req, res) {
+  res.send("GetAll");
 });
 
-app.put('/users', function (req, res) {
-  res.send('Atualizando usuário');
+app.put("/users", function (req, res) {
+  res.send("Atualizando usuário");
 });
 
-app.delete('/users', function (req, res) {
-  res.send('usuário deletado');
+app.delete("/users", function (req, res) {
+  res.send("usuário deletado");
 });
 
 /*--------------------------search----------------------------*/
 
-app.get('/search', function (req, res) {
+app.get("/search", function (req, res) {
   res.send("pegando publicações");
 });
 
