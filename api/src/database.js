6f7b426622cfdb0bf3a6c0344d7d93cd0a2e12e3
Arthur Telles <arthurtelles19@gmail.com>
2024-01-24 15:35:05 -0300

    atualização 24/01

--- api/src/database.js
+++ api/src/database.js
@@ -3,7 +3,7 @@ const path = require("path");
 const knex = require("knex")({
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, "database.sqlite3")
+        filename: path.resolve(__dirname, "appcaronas.sqlite3")
     },
     useNullAsDefault: true,
 });
