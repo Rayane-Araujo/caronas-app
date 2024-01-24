72137db30eb98aad81b5c9f331bab16b68d0050d
Arthur Telles <arthurtelles19@gmail.com>
2024-01-24 18:49:35 -0300

    .

--- api/src/routes.js
+++ api/src/routes.js
@@ -7,13 +7,11 @@ const usersController = require("./controllers/users");
 const appRoutes = express.Router();
 
 // ROTAS USERS
-
 appRoutes.post("/users", usersController.create);
 appRoutes.get("users", usersController.getAll);
 appRoutes.delete("users", usersController.delete);
 
 //ROTAS REQUEST
-
 appRoutes.post("/request", requestController.create);
 appRoutes.get("/request", requestController.getAll);
 appRoutes.put("request", requestController.put);
@@ -21,11 +19,9 @@ appRoutes.delete("request", requestController.delete);
 
 
 //ROTAS SEARCH
-
 appRoutes.get("/search", searchController.getAll);
 
 //ROTAS OFFER
-
 appRoutes.post("/offer", offerController.create);
 appRoutes.get("/offer", offerController.getAll);
 appRoutes.put("/offer", offerController.put);
