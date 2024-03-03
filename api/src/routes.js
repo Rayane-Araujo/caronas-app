b9650a4596ae27492be5b7c4761739947791f3b9
Arthur Telles <arthurtelles19@gmail.com>
2024-03-03 15:39:10 -0300

    Remover os flow não usados da api

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const knex = require("./database");
 const rideController = require("./controllers/ride");
-const requestController = require("./controllers/request");
-const searchController = require("./controllers/search");
 const usersController = require("./controllers/users");
 const loginController = require("./controllers/login");
 const appRoutes = express.Router();
@@ -11,15 +9,6 @@ const appRoutes = express.Router();
 appRoutes.post("/users", usersController.create);
 appRoutes.get("/users", usersController.getAll);
 
-
-//ROTAS REQUEST
-appRoutes.post("/request", requestController.create);
-appRoutes.get("/request", requestController.getAll);
-
-
-
-//ROTAS SEARCH
-appRoutes.get("/search", searchController.getAll);
 
 //ROTAS RIDE
 appRoutes.post("/ride", rideController.create);
