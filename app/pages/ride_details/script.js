812a0009f67c4d72cf5d48bcfeb23535dc7f54b6
Rayane Araújo <rayanearaujo28@hotmail.com>
2024-03-05 20:06:27 -0400

    resolvendo bug do pop-up  da tela detalhes da carona

--- app/pages/ride_details/script.js
+++ app/pages/ride_details/script.js
@@ -1,10 +1,11 @@
 const button = document.querySelector("#button_interest")
 const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector(".button-close")
 
 button.onclick = function(){
     modal.showModal()
 }
 
-document.querySelector(".button-close").addEventListener('click',function(){
-    document.querySelector('#overlay').style.display = 'none';
-  })
+buttonClose.onclick = function(){
+    modal.close()
+}
