d302384da05298a616858db8aa083b9b763e22a1
Rayane Araújo <rayanearaujo28@hotmail.com>
2024-03-14 16:47:25 -0400

    Integrando lista de caronas

--- app/pages/ride_list/script.js
+++ app/pages/ride_list/script.js
@@ -1,5 +1,35 @@
-
 document.querySelector('.content-button button').addEventListener('click', function (){
+
     navigate('ride_create');
 })
+
+const url = "http://localhost:3333/ride"
+// const newList = {
+//     name: "Rayane",
+//     time: "13:00",
+//     date: "20/01/1999"
+// }
+
+function getRideList(){
+    axios.get(url)
+    .then(response =>{
+       const data = response.data
+       rideList.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
+}
+getRideList()
+
+// "Cadastrando nova lista na API"
+// function addNewList(){
+//     axios.post(url, newList)
+//     .then(response => {
+//         console.log(response.data)
+//     })
+//     .catch(error => console.log(error))
+// }
+// addNewList()
+
+
+
 
