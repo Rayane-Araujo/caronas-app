d7bffb8af2ca9c3861a70c77ae9cea6050580374
Vinicius <vinicius.borges95@hotmail.com>
2024-03-14 22:53:14 -0300

    integração tela de detalhes API

--- app/pages/ride_details/script.js
+++ app/pages/ride_details/script.js
@@ -9,3 +9,41 @@ button.onclick = function(){
 buttonClose.onclick = function(){
     modal.close()
 }
+
+document.querySelector("button").addEventListener("click", function () {
+    const valueInputOrigin = document.querySelector("#origin").value;
+    const valueInputDestiny = document.querySelector("#destiny").value;
+    const valueInputHour = document.querySelector("#hour").value;
+    const valueInputDate = document.querySelector("#date").value;
+    const valueTextFieldDescribe = document.querySelector("#textFieldDescribe").value;
+  
+    console.log({
+      origin: valueInputOrigin,
+      destiny: valueInputDestiny,
+      hour: valueInputHour,
+      date: valueInputDate,
+      textField: valueTextFieldDescribe,
+    });
+    
+    const formData = {
+      valueInputOrigin,
+      valueInputDestiny,
+      valueInputDate,
+      valueInputHour,
+      valueTextFieldDescribe,
+    };
+  
+    axios
+      .post("http://localhost:3333/ride", formData)
+  
+      .then(function (response) {
+        //Lida com a resposta do servidor
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        //Lida com erros da requisição
+        console.error(error);
+      });
+  
+  });
+  
