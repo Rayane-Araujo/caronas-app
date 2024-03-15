19935a1b9680a1b21b06e853ecb454a187cb7031
Diego ZF <ddzadravec@gmail.com>
2024-03-15 17:12:07 -0300

    feat: terminate integration ride details

--- app/pages/ride_details/script.js
+++ app/pages/ride_details/script.js
@@ -1,49 +1,55 @@
-const button = document.querySelector("#button_interest")
-const modal = document.querySelector("dialog")
-const buttonClose = document.querySelector(".button-close")
+axios
+  .get("http://localhost:3333/ride/1")
+  .then(function (response) {
+    //Lida com a resposta do servidor
+    console.log(response.data.dataRes);
 
-button.onclick = function(){
-    modal.showModal()
-}
+    const data = response.data.dataRes;
 
-buttonClose.onclick = function(){
-    modal.close()
-}
+    const content = `
+      <div class="container-descricao-detalhes-carona">
+        <span class="descricao-detalhe">Condutor: ${data.name}</span>
+        <span class="descricao-detalhe">Veículo: ${data.vehicle}</span>
+      </div>
 
-document.querySelector("button").addEventListener("click", function () {
-    const valueInputOrigin = document.querySelector("#origin").value;
-    const valueInputDestiny = document.querySelector("#destiny").value;
-    const valueInputHour = document.querySelector("#hour").value;
-    const valueInputDate = document.querySelector("#date").value;
-    const valueTextFieldDescribe = document.querySelector("#textFieldDescribe").value;
-  
-    console.log({
-      origin: valueInputOrigin,
-      destiny: valueInputDestiny,
-      hour: valueInputHour,
-      date: valueInputDate,
-      textField: valueTextFieldDescribe,
-    });
-    
-    const formData = {
-      valueInputOrigin,
-      valueInputDestiny,
-      valueInputDate,
-      valueInputHour,
-      valueTextFieldDescribe,
-    };
-  
-    axios
-      .post("http://localhost:3333/ride", formData)
-  
-      .then(function (response) {
-        //Lida com a resposta do servidor
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        //Lida com erros da requisição
-        console.error(error);
-      });
-  
+      <h4>Observações</h4>
+      <p class="descricao-detalhe" id="textFieldDescribe">${data.observation}</p>
+
+      <h4>Oferecendo carona de:</h4>
+
+      <div>
+        <div class="line_content">
+          <img src="../images/Line 12.png" />
+        </div>
+
+        <div class="locate_content">
+          <div class="origin" id="origin">
+            <span>${data.origin}</span>
+          </div>
+
+          <div class="destiny" id="destiny">
+            <span><span>${data.destiny}</span></span>
+            <span class="descricao-detalhe data">
+              <span id="date">Data: ${data.date}</span
+              ><span id="hour">ás ${data.time}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      `;
+
+    document.querySelector(".content_info").innerHTML = content;
+  })
+  .catch(function (error) {
+    //Lida com erros da requisição
+    console.error(error);
   });
-  
+
+const modal = document.querySelector("dialog");
+
+document
+  .querySelector("#button_interest")
+  .addEventListener("click", () => modal.showModal());
+document
+  .querySelector(".button-close")
+  .addEventListener("click", () => modal.close());
